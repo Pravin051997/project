a50b7d3edc6e72d0a20af939bbbc7e5e449fd877
Jerry30 <imnaveen29@gmail.com>
2019-07-21 20:23:06 +0530

    source

---
+++ Temperature/src/project/Temperature.java
@@ -0,0 +1,29 @@
+package project;
+import java.util.Scanner;
+public class Temperature {
+	  static double convertToFarenheit(double c)
+		{
+			return (c*9/5)+32;
+		}
+		  static double convertToCelsius(double f)
+		{
+			return (f-32)*5/9;
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Celsius =" );
+		double c,f;
+		c=sc.nextDouble();
+		System.out.println("Fahrenheit =" );
+		f=sc.nextDouble();
+		System.out.println(" Convert To Fahrenheit ="+convertToFarenheit(c));
+
+		System.out.println(" Convert To Celsius ="+convertToCelsius(f));
+		sc.close();
+
+	}
+
+}
---
+++ Temperature/src/project/TemperatureTest.java
@@ -0,0 +1,19 @@
+package project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TemperatureTest {
+
+	@Test
+	void test() {
+		Temperature temp = new  Temperature();
+		double out=temp.convertToFarenheit(0);
+		assertEquals(32,out);
+		
+		double out1 = temp.convertToCelsius(32);
+		assertEquals(0,out1);
+	}
+
+}
